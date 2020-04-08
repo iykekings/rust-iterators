edff1056d92f3aa327c6d3b99730404cbb886505
iykekings <iykekings36@gmail.com>
2020-04-08 14:18:01 +0100

    map, filter, filter_map, peekable

--- src/basic.rs
+++ src/basic.rs
@@ -63,4 +63,51 @@ mod test {
     assert_eq!(iter.next(), Some((&3, &2)));
     assert_eq!(iter.next(), None);
   }
+
+  #[test]
+  fn iter_map() {
+    // Takes a closure and creates an iterator which calls that closure on each element.
+    let a1 = [2, 4, 5, 6, 7];
+    let mut iter = a1.iter().map(|x| x * 3);
+    assert_eq!(iter.next(), Some(6));
+    assert_eq!(iter.next(), Some(12));
+    assert_eq!(iter.next(), Some(15));
+    assert_eq!(iter.next(), Some(18));
+    assert_eq!(iter.next(), Some(21));
+  }
+
+  #[test]
+  fn iter_filter() {
+    // Creates an iterator which uses a closure to determine if an element should be yielded.
+    let a = vec![1, 2, 3, 4, 5, 6];
+    a.iter()
+      .enumerate()
+      .filter(|&(_, &x)| x % 2 == 0)
+      .for_each(|(i, x)| assert_eq!(x, &a[i]));
+  }
+
+  #[test]
+  fn iter_filter_map() {
+    // Creates an iterator that both filters and maps.
+    // closure must return an Option<T>
+    let a = ["1", "lol", "3", "NaN", "5"];
+
+    let mut iter = a.iter().filter_map(|s| s.parse().ok());
+
+    assert_eq!(iter.next(), Some(1));
+    assert_eq!(iter.next(), Some(3));
+    assert_eq!(iter.next(), Some(5));
+    assert_eq!(iter.next(), None);
+  }
+
+  #[test]
+  fn iter_peekable() {
+    let a = vec![1, 2, 3, 4];
+    let mut iter = a.iter().peekable();
+
+    assert_eq!(iter.peek(), Some(&&1));
+    assert_eq!(iter.next(), Some(&1));
+    assert_eq!(iter.next(), Some(&2));
+    assert_eq!(iter.peek(), Some(&&3));
+  }
 }
